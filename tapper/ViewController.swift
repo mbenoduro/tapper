ae558bd25448151b6cf14c138bae028cd9dcfe8f
Maurice Ben-Oduro <moduro@iocreate.attlocal.net>
2016-07-15 17:42:35 -0400

    Initial commit for first game.

--- tapper/ViewController.swift
+++ tapper/ViewController.swift
@@ -9,17 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Properties
+    var maxTaps: Int = 0
+    var currentTaps: Int = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func playBtnPressed(sender: UIButton!){
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+            
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            updateTapsLbl()
+            
+        }
+    }
+    
+    @IBAction func coinBtnPressed(sender: UIButton!){
+        
+        currentTaps += 1
+        
+        updateTapsLbl()
+        
+        if isGameOver(){
+            restartGame()
+        }
+    }
+    
+    func updateTapsLbl(){
+        tapsLbl.text = "\(currentTaps) Taps"
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        howManyTapsTxt.text = ""
+        
+        logoImg.hidden = false
+        howManyTapsTxt.hidden = false
+        playBtn.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
-
+    
 
 }
 
